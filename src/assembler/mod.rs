8d989c0e610c526b9a6fd49ef93eb49a5c7fd83d
Max Tottenham <mtottenh@akamai.com>
2017-02-05 14:27:52 +0000

    Create a dispatch table for our assembler.

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -2,8 +2,80 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
 use std::collections::BTreeMap;
-
+use std::collections::HashMap;
 mod tokeniser;
+type InstrAssembleFn = fn(&tokeniser::TokenisedLine) -> u32;
+
+const i_type_instr : [&'static str; 12] = [ "addi", "subi", "muli", "lw", "sw", "beq", "bne",
+"blt", "bgt", "ble", "bge", "mov" ];
+const r_type_instr : [&'static str; 5] = [ "add", "sub", "mul", "jr", "out" ];
+const j_type_instr : [&'static str; 1] = [ "jmp" ];
+
+
+fn assemble_i_type (line: &tokeniser::TokenisedLine) -> u32 {
+    println!("Assembling I type instruction");
+    return 1;
+}
+
+fn assemble_j_type (line: &tokeniser::TokenisedLine) -> u32 {
+    println!("Assembling J type instruction");
+    return 1;
+}
+
+fn assemble_r_type (line: &tokeniser::TokenisedLine) -> u32 {
+    println!("Assembling R type instruction");
+    return 1;
+}
+
+fn assemble_fill (line: &tokeniser::TokenisedLine) -> u32 {
+
+    return 1;
+}
+fn assemble_skip (line: &tokeniser::TokenisedLine) -> u32 {
+
+    return 1;
+}
+
+fn assemble_halt (line: &tokeniser::TokenisedLine) -> u32 {
+
+    return 1;
+}
+
+struct InstrFactory {
+    table: HashMap<String, InstrAssembleFn>
+}
+
+impl InstrFactory {
+    fn new () -> InstrFactory {
+        let mut i = InstrFactory { table: HashMap::new() };
+
+
+        for ins in i_type_instr.iter() {
+            i.table.insert(ins.to_string(),assemble_i_type);
+        }
+        for ins in r_type_instr.iter() {
+            i.table.insert(ins.to_string(),assemble_r_type);
+        }
+        for ins in j_type_instr.iter() {
+            i.table.insert(ins.to_string(),assemble_j_type);
+        }
+
+        // Assembler directives
+        i.table.insert(".fill".to_string(), assemble_fill);
+        i.table.insert(".skip".to_string(), assemble_skip);
+        i.table.insert("halt".to_string(), assemble_halt);
+        return i
+    }
+    fn create (&self, ins: &String, line: &tokeniser::TokenisedLine) -> u32 {
+        println!("Assembling op: {}", ins);
+        if self.table.contains_key(ins) {
+            self.table[ins](line);
+        } else {
+            panic!("Invalid instruction {}", ins);
+        }
+        return 1;
+    }
+}
 
 
 
@@ -13,6 +85,7 @@ pub fn assemble (input: String, output: String) {
     let f = File::open(&input).unwrap();
     let reader = BufReader::new(f);
     let lines = reader.lines();
+   
     /* Pass 1 - Store addresses of labels in a symbol table */
     let mut sym_tab = BTreeMap::new();
     let mut address : u32 = 0;
@@ -41,10 +114,24 @@ pub fn assemble (input: String, output: String) {
     }
 
     println!("Symbol Table: {:?}", sym_tab);
-    let f = File::open(input).unwrap();
+
+    let f = File::open(&input).unwrap();
     let reader = BufReader::new(f);
     let lines = reader.lines();
+    let fac = InstrFactory::new();
+    /* Pass 2 - assemble instructions */
     for l in lines {
-        
+         let t = tokeniser::tokenise_line(l.unwrap());
+         match &t {
+            &None => (),
+            &Some(ref tokens) => {
+                match &tokens.opcode {
+                    &None => (),
+                    &Some(ref op) => {
+                        fac.create(op,tokens);
+                    }
+                }
+            }
+         }
     }
 }
